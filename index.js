d7edd0f138052f50787c1fce44eb33ecb8d8a660
Haley Mendoza <mendozahaley@gmail.com>
2018-06-03 14:10:59 -0700

    adds quit command and on.error.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// require('dotenv').config();
+
 // First Party Modules
 const EventEmitter = require('events');
 const net = require('net');
@@ -20,13 +22,8 @@ let User = function(socket) {
   this.socket = socket;
 };
 
+// Connection listener. Uses obhect for socket pool to make references to it O(1)
 
-/**
- * Connection listener.
- * When the user first connects, create a new user instance for them, noting the socket
- * Add them into the pool, by id.  We're using an object for the socket pool here
- * to make any lookups, or write operations O(1) instead of O(n)
- */
 server.on('connection', (socket) => {
   console.log('CONNECTION!');
   let user = new User(socket);
@@ -34,22 +31,20 @@ server.on('connection', (socket) => {
   socket.on('data', (buffer) => dispatchAction(user.id, buffer));
 });
 
+server.on('error', () => {
+  console.log('error occured');
+
+});
+
 /**
  * Command Parser
- * Handles commands such as:
- *    @all message
- *    @nick newname
- *    @quit
- *    @list
- *    @dm username message
- * @param buffer
- * @returns {*}
- *    format:
+ *    Format:
  *      command: the @command given
  *      payload: the full text after the @command
  *      target: the first word after the @command (might be useful for @dm, @kick)
  *      message: all of the words after "target" (only useful for @dm)
  */
+
 let parse = (buffer) => {
 
   let text = buffer.toString().trim();
@@ -60,25 +55,16 @@ let parse = (buffer) => {
 
 };
 
-/**
- * Dispatcher -- parses the command buffer and then triggers an event with:
- *    command
- *    parsed entry object
- *    and the current user's id
- * @param userId
- * @param buffer
- */
+// Dispatcher -- parses the command buffer and then triggers an event with: command,parsed entry object, and the current user's id
+
+
 let dispatchAction = (userId, buffer) => {
   let entry = parse(buffer);
   entry && eventEmitter.emit(entry.command, entry, userId);
 };
 
-/**
- * Command Handlers.
- * These respond when events are triggered (emitted) that match their "on" clause
- * This patter rocks ... you can add/remove command support without adding any
- * conditional logic, just listeners
- */
+
+// Command Handlers
 
 eventEmitter.on('@all', (data, userId) => {
   for( let key in socketPool ) {
@@ -100,14 +86,23 @@ eventEmitter.on('@list', (data, userId) => {
 });
 
 eventEmitter.on('@dm', (data, userId) => {
+  let recipient = data.target;
+  let message = data.message;
+  let sender = socketPool[userId].nickname;
 
-  // data.target == who it's going to
-  // data.message == the message
-  // find socketPool[target].socket.write(message);
+  for(let key in socketPool){
+    let candidate = socketPool[key];
+    if(candidate.nickname === recipient){
+      candidate.socket.write(`From ${sender}: ${message}`);
+      break;
+    }
+  }
 });
 
 eventEmitter.on('@quit', (data, userId) => {
-// end
+  let user = socketPool[userId];
+  user.socket.destroy();
+  delete socketPool[userId];
 });
 
 server.listen(port, () => {
